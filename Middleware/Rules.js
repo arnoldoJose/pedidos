efc125ee5fec3ff12c39afd067686af6d07a5240
arnoldoJose <aolivasgonzales73@gmail.com>
2020-12-23 16:48:38 -0600

    roles change

--- Middleware/Rules.js
+++ Middleware/Rules.js
@@ -6,7 +6,6 @@ let consultRol = (req,res,next) => {
   let {rol} = req.usuario;
 
   if (rol === "ADMIN_ROLE") {
-    res.json({ message: "ERES administardor" });
     next();
   } else {
     res.json({ message: "No eres administardor",rol: rol });
