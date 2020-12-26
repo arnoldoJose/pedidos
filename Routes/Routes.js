dcaddf222fd97511c8946d6d29a86a7e016ba065
arnoldoJose <aolivasgonzales73@gmail.com>
2020-12-26 08:14:08 -0600

    prueba otra ves

--- Routes/Routes.js
+++ Routes/Routes.js
@@ -115,8 +115,9 @@ route.get("/obtener/producto/:id", async(req,res)=>{
 // subir el archivo
 
 let uploadFile  = (res) => {
-
-  res.status(301).json({err: "mmmmmmm"});
+  Fs.mkdir("Files",(err) => {
+    if(err) return res.json({ message : "verificar bien"});
+  })
 
   //  Fs.mkdir("Upload", (err) => {
   //    return res.status(401).json({ err: "el directirio no se creo" });
