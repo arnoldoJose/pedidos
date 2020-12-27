0830ae84e2868612524f485b99ea38952b1e613b
arnoldoJose <aolivasgonzales73@gmail.com>
2020-12-26 21:22:18 -0600

    added new folder cv

--- Config/fileUp.js
+++ Config/fileUp.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: function (req,file,cb) {
-    cb(null,'Upload/images')
+    cb(null,'./Upload/images')
   },
   filename: function (req,file,cb) {
     cb(null, `${file.fieldname}-${Date.now()}`)
