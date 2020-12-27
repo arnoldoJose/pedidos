ccac9736f6cf64ad582519e5f9840555f61b87d2
arnoldoJose <aolivasgonzales73@gmail.com>
2020-12-26 20:32:02 -0600

    added

--- Config/fileUp.js
+++ Config/fileUp.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: function (req,file,cb) {
-    cb(null, __dirname+'../Upload')
+    cb(null, __dirname+'../../Upload')
   },
   filename: function (req,file,cb) {
     cb(null, `${file.fieldname}-${Date.now()}`)
